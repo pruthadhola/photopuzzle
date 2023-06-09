d7030cd14baeaf0596407a4f465c046ea7a887b1
Dell <pruthdhola22@gmail.com>
2023-06-09 17:54:06 +0530

    Initial commit

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+class Data {
+  static List photo = [
+    "pic1.jpg",
+    "pic2.jpg",
+    "pic3.jpg",
+    "pic4.jpg",
+    "pic5.jpg",
+    "pic6.jpg",
+    "pic7.jpg",
+    "pic8.jpg",
+
+  ];
+}
